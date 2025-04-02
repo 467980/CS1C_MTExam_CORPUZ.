1100b7a05f9a929efed8a2c1a6fa6f3c83d6da80
467980 <benedickcorpuz274@gmail.com>
2025-04-02 14:29:24 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,5 @@
+let studentName = prompt("Enter your name:");
+let subiectTitle = prompt("Enter the subject title:");
+let classschedule = prompt("Enter the class schedule (Time, Days):");
+let classroom = prompt("Enter the classroom number:");
+let classInstructor = prompt("Enter the class instructor's name:");
